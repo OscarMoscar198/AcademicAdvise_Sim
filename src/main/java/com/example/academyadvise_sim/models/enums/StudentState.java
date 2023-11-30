8fb1b24b03e438c142bd31fbc99f28f0d19700dc
Luis Brodely <luisbrodely@advantedigital.com>
2023-11-29 22:34:11 -0600

    UPS

--- src/main/java/com/example/academyadvise_sim/models/enums/StudentState.java
+++ src/main/java/com/example/academyadvise_sim/models/enums/StudentState.java
@@ -3,7 +3,7 @@ package com.example.academyadvise_sim.models.enums;
 public enum StudentState {
     WAIT,
     SIT_WITHOUT_ORDER,
-    WAIT_ORDER,
+    WAIT_ADVICE,
     EAT,
     EAT_FINISH,
     EXIT
